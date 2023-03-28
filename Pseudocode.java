fa0eec0b10c17ae7bf13e2c0a22bdef028e7d238
AJITHA1996 <129102760+AJITHA1996@users.noreply.github.com>
2023-03-28 08:25:43 +0530

    Add files via upload

---
+++ Pseudocode.java
@@ -0,0 +1,26 @@
+package week1;
+
+public class Pseudocode {
+
+	public static void main(String[] args) {
+		
+		//1. Initialize an integer with a negative number like, int number = -40;
+		
+		int num = -40;
+		
+		//2. Check if the number is a negative number 
+		  // Hint : any number that is lesser than zero is a negative number
+		
+		if(num<0) {
+			//3. If so, convert the number to a positive number
+			num = num * -1;
+			
+		//	4. Print as below  "The number -40 is converted to 40"
+			
+			System.out.println("The number -40 is converted to 40");
+		}
+				
+
+	}
+
+}
